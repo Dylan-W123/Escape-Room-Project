e400c74cf298cd83377392040aaef1bda262fa69
dylan walsh <k00256231@student.lit.ie>
2023-01-24 01:50:15 +0000

    FINAL STAGE

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] private int score;
+    [SerializeField] private int finalScore;
+    [SerializeField] private UnityEvent winAction;
+
+    public void UpdateScore(){
+        score++;
+        if (score == finalScore)
+        {
+            winAction.Invoke();
+        }
+    }
+}
